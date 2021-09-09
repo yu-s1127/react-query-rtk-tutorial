5bbd207b2f9860b4a02d2a439408a73048883298
yuhei_suzuki <yuhei_suzuki@yuhei-suzukinoMacBook-Pro.local>
2021-09-09 17:25:45 +0900

    react query

--- frontend/src/components/ClassicalFetchA.tsx
+++ frontend/src/components/ClassicalFetchA.tsx
@@ -8,16 +8,19 @@ export const ClassicalFetchA: VFC = () => {
   const history = useHistory()
   const { isLoading, isError, tasks } = useClassicalFetch()
   console.log('rendered ClassicalFetchA')
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (isError) {
+    return <div>Error</div>
+  }
 
   return (
     <div className="flex flex-col justify-center items-center">
       <p className="text-center font-bold mb-3">ClassicalFetchA</p>
-      {isError && <div>Error</div>}
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        tasks?.map((task) => <p key={task.id}>{task.title}</p>)
-      )}
+      {tasks?.map((task) => (
+        <p key={task.id}>{task.title}</p>
+      ))}
       <ChevronDoubleRightIcon
         onClick={() => history.push('/fetch-b')}
         className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
---
+++ frontend/src/components/ReactQueryA.tsx
@@ -0,0 +1,27 @@
+import { VFC } from 'react'
+import { useQueryTasks } from '../hooks/useQueryTasks'
+import { useHistory } from 'react-router'
+import { ChevronDoubleRightIcon } from '@heroicons/react/solid'
+
+export const ReactQueryA: VFC = () => {
+  console.log("rendered ReactQueryA")
+  const history = useHistory()
+  const { status, data } = useQueryTasks()
+
+  if (status === 'loading') return <div>{'Loading...'}</div>
+  if (status === 'error') return <div>{'Error'}</div>
+
+  return (
+    <>
+      <p className="font-bold my-3">ReactQueryA</p>
+      {data?.map((task) => (
+        <p key={task.id}>{task.title}</p>
+      ))}
+      <ChevronDoubleRightIcon
+        onClick={() => history.push('/query-b')}
+        className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
+      />
+      <p className="text-sm">react query B</p>
+    </>
+  )
+}
